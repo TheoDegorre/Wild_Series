77dd4c64ed15e1d5d60855f70b3afbe35986bfb3
TheoDegorre <degorret@gmail.com>
2024-05-24 10:33:41 +0200

    harmonia quest3

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,25 @@ const express = require("express");
 
 const router = express.Router();
 
+const sayWelcome = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+router.get("/", sayWelcome);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
+
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
+
 
 /* ************************************************************************* */
 
