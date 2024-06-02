68bd5f0628b86403b8b4cf9e52146b850d825d14
TheoDegorre <degorret@gmail.com>
2024-06-02 14:15:41 +0200

    complete script for database category and program

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,27 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
 create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+    id int unsigned primary key auto_increment not null,
+    title varchar(255) not null,
+    user_id int unsigned not null,
+    foreign key (user_id) references user(id)
+);
+
+create table category (
+    id int unsigned primary key auto_increment not null,
+    name varchar(255) not null
+);
+
+create table program (
+    id int unsigned primary key auto_increment not null,
+    title VARCHAR(255) not null,
+    synopsis text not null,
+    poster VARCHAR(255) not null,
+    country VARCHAR(255) not null,
+    year int,
+    Foreign Key (category_id) REFERENCES category(id)
 );
