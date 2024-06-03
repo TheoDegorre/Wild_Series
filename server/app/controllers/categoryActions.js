a713b8b4808715e86982698fa7ebbfa5f07b825e
TheoDegorre <degorret@gmail.com>
2024-06-03 11:11:53 +0200

    fixtures

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,42 +1,34 @@
 // Some data to make the trick
+const tables = require("../../database/tables");
 
 const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
-  
-  // Declare the actions
-  
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredCategories= categories.filter((category) =>
-        category.synopsis.includes(req.query.q)
-      );
-  
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
-  };
-  
+  {
+    id: 1,
+    name: "Science-Fiction",
+  },
+  {
+    id: 2,
+    name: "Comédie",
+  },
+];
+
+// Declare the actions
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+  res.json(categoriesFromDB);
+};
   const read = (req, res) => {
-    const parsedId = parseInt(req.params.id, 10);
-  
-    const category = categories.find((p) => p.id === parsedId);
-  
-    if (category != null) {
-      res.json(category);
-    } else {
-      res.sendStatus(404);
-    }
-  };
-  
-  // Export them to import them somewhere else
-  
-  module.exports = { browse, read };
+  const parsedId = parseInt(req.params.id, 10);
+  const category = categories.find((p) => p.id === parsedId);
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+
+};
+/* Here you code */
+
+// Export them to import them somewhere else
+
+module.exports = { browse, read};
--- server/app/controllers/programsActions.js
+++ server/app/controllers/programsActions.js
@@ -1,4 +1,5 @@
 // Some data to make the trick
+const tables = require("../../database/tables");
 
 const programs = [
   {
@@ -23,32 +24,21 @@ const programs = [
   },
 ];
 
-// Declare the actions
+// Declare the action
+const browse = async (req, res) => {
+  const programsFromDB = await tables.program.readAll();
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q)
-    );
-
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+  res.json(programsFromDB);
 };
-
 const read = (req, res) => {
   const parsedId = parseInt(req.params.id, 10);
-
   const program = programs.find((p) => p.id === parsedId);
-
   if (program != null) {
     res.json(program);
   } else {
     res.sendStatus(404);
   }
 };
-
-// Export them to import them somewhere else
+// Export it to import it somewhere else
 
 module.exports = { browse, read };
